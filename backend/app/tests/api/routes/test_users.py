8d14fb463413b2b35b3d5edede9f554f4be96237
william chandra <williamchans57@gmail.com>
2025-11-03 19:46:56 +0700

    fix email sending

--- backend/app/tests/api/routes/test_users.py
+++ backend/app/tests/api/routes/test_users.py
@@ -38,7 +38,7 @@ def test_create_user_new_email(
     client: TestClient, superuser_token_headers: dict[str, str], db: Session
 ) -> None:
     with (
-        patch("app.utils.send_email", return_value=None),
+        patch("app.utils.email.send_email", return_value=None),
         patch("app.core.config.settings.SMTP_HOST", "smtp.example.com"),
         patch("app.core.config.settings.SMTP_USER", "admin@example.com"),
     ):
